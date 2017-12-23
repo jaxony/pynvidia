bbd62a6092ac65f15a474d887bdc6139d8804e20
Jackson Huang <jackson_huang@live.com>
2017-12-23 15:24:22 +1100

    Added setup files for pip

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(name='pyvidia',
+      version='0.1.0',
+      description='NVIDIA GPU utilities for Python',
+      author='Jackson Huang',
+      author_email='jackson_huang@live.com',
+      url='https://github.com/jaxony/pyvidia',
+      license='MIT',
+      packages=['pyvidia']
+      keywords=[
+          'Utilities',
+          'Deep Learning',
+          'NVIDIA',
+          'GPU'
+      ])
